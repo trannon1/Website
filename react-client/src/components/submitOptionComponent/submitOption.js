202443402e0931f320e93cc7aa80db5324db1b2a
Bryan Tran <tran_b1@denison.edu>
2019-01-11 11:19:01 -0800

    Working on submit orders

--- react-client/src/components/submitOptionComponent/submitOption.js
+++ react-client/src/components/submitOptionComponent/submitOption.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
+import "./submitOption.css"
 import $ from "jquery";
 
-class SubmitOptionPage extends Component {
-    
-    /* When the user clicks on the button, 
-    toggle between hiding and showing the dropdown content */
-    myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-
+class SubmitOptionPage extends React.Component {
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {isToggleOn: true};
+  
+    //   // This binding is necessary to make `this` work in the callback
+    //   this.handleClick = this.handleClick.bind(this);
+    // }
+  
+    // handleClick() {
+    //     document.getElementById("myDropdown").classList.toggle("show");
+    // }
+  
     render() {
       return (
-        <div>
-          <div class="dropdown">
-              <button onclick={this.myFunction()} class="dropbtn">Dropdown</button>
-              <div id="myDropdown" class="dropdown-content">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-              </div>
-          </div>
-        </div>  
-      )
+        <div class="dropdown">
+            <button onclick={this.handleClick} class="dropbtn">Dropdown</button>
+            <div id="myDropdown" class="dropdown-content">
+                <a href="#home">Home</a>
+                <a href="#about">About</a>
+                <a href="#contact">Contact</a>
+            </div>
+        </div>
+      );
     }
   }
+
+// class SubmitOptionPage extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {isToggleOn: true};
+    
+//         // This binding is necessary to make `this` work in the callback
+//         this.handleClick = this.handleClick.bind(this);
+//       }
+    
+//       handleClick() {
+//         this.setState(state => ({
+//           isToggleOn: !state.isToggleOn
+//         }));
+//       }
+
+//     render() {
+//       return (
+//         <div>
+//           <div class="dropdown">
+//               <button onclick={this.handleClick()}>
+//               {this.state.isToggleOn ? 'ON' : 'OFF'}
+//               </button>
+//               {/* <div id="myDropdown" class="dropdown-content">
+//                   <a href="#">Link 1</a>
+//                   <a href="#">Link 2</a>
+//                   <a href="#">Link 3</a>
+//               </div> */}
+//           </div>
+//         </div>  
+//       )
+//     }
+//   }
   
   export default SubmitOptionPage;
